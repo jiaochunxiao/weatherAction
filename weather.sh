a86b5ca096092b0c3c0fa8483856c1808e968eed
jiaochunxiao <jcx13627607951@gmail.com>
2021-11-15 20:16:30 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -4,8 +4,8 @@ set -eux
 CITY=Beijing
 LANGUAGE="zh-CN"
 UNIT=m
-UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
-
+# UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+UA="Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
